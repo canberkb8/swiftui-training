bb704b8b1f47a68c7e6c822593d2ee8304d227ef
Canberk BİBİCAN <canberkbibican@hotmail.com>
2024-05-31 16:13:08 +0300

    Initial Commit

---
+++ HundredDaySwiftUI/HundredDaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDaySwiftUIApp.swift
+//  HundredDaySwiftUI
+//
+//  Created by Canberk Bibican on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HundredDaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
